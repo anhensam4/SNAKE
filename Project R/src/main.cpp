410dd3aec36ef0e83f4a4b28046f8c51dff5b064
anhensam4 <89511405+anhensam4@users.noreply.github.com>
2022-03-14 18:45:17 +0700

    kb event

---
+++ Project R/src/main.cpp
@@ -0,0 +1,112 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+#include <vector>
+
+
+#include "RenderWindow.hpp"
+#include "Entity.hpp"
+#include "Math.hpp"
+#include "Utils.hpp"
+
+using namespace std;
+
+int main(int argc, char* args[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO) > 0)
+	{
+		cout << "VIDEO ERROR : " << SDL_GetError() << endl;
+	}
+	if (!(IMG_Init(IMG_INIT_PNG)))
+	{
+		cout << "IMG ERROR : " << SDL_GetError() << endl;
+	}
+
+	RenderWindow window("Game v1.0", 1280, 720);
+
+	SDL_Texture* grassTexture = window.loadTexture("res/gfx/ground_grass_1.png");
+
+	vector<Entity> entities;
+
+	int s = 0;
+
+	cin >> s;
+
+	for(int i=0; i<s; i++)
+	{
+		Vector2f tempPos;
+		cin >> tempPos.x >> tempPos.y;
+		Entity tempE(tempPos, grassTexture);
+		entities.push_back(tempE);
+	}
+
+	bool gameRunning = true;
+
+	SDL_Event event;
+
+	const float timeStep = 0.01f;
+	float accumulator = 0.0f;
+	float currentTime = utils::hireTimeInSeconds();
+
+	while(gameRunning)
+	{
+		int startTicks = SDL_GetTicks();
+
+		float newTime = utils::hireTimeInSeconds();
+		float frameTime = newTime - currentTime;
+
+		currentTime = newTime;
+
+		accumulator += frameTime;
+
+		while(accumulator >= timeStep)
+		{
+			while (SDL_PollEvent(&event))
+			{
+				if (event.type == SDL_QUIT)
+				{
+					gameRunning = false;
+				}
+
+				if(event.type == SDL_MOUSEMOTION)
+				{
+					cout << "mouse moved" << endl;
+				}
+
+				if(event.type == SDL_KEYDOWN)
+				{
+					cout << "key pressed" << endl;
+					if(event.key.keysym.sym == SDLK_0)
+					{
+						cout << "0 was pressed" << endl;
+					}
+					else
+					{
+						cout << "0 wasnt pressed" << endl;
+					}
+				}
+			}
+
+			accumulator -= timeStep;
+		}
+
+		const float alpha = accumulator / timeStep;
+
+		window.clear();
+
+		window.display();
+
+		int frameTicks = SDL_GetTicks() - startTicks;
+
+		if(frameTicks < 1000 / window.getRefreshRate())
+		{
+			SDL_Delay(1000 / window.getRefreshRate() - frameTicks);
+		}
+
+	}
+
+	window.cleanUp();
+	SDL_Quit();
+
+	return 0;
+}
